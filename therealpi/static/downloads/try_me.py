dd840193de07b2ed289beaf7b3134b528feb6203
MANA624 <pivotman624@gmail.com>
2019-05-18 08:26:51 -0600

    Added one last file

---
+++ therealpi/static/downloads/try_me.py
@@ -0,0 +1,21 @@
+# I like you!
+start_num = 1234
+
+for i in range(19)
+start_num *= 3
+start_num -= 4
+
+
+start_num %= 109
+start_num += datetime.datetime.today().year
+
+
+while start_num < 10000
+start_num *= 6/5
+start_num -= 20
+
+
+start_num = ceil(start_num / 1234)
+start_num ** 4
+
+print(start_num)
